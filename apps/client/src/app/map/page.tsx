718006c12af7456a10b1f1cac0ee2c7edba785ac
Shion <seoy1108@gmail.com>
2025-07-02 16:17:59 +0900

    feat: 메인 페이지 및 /map 페이지 라우팅 설정

--- apps/client/src/app/map/page.tsx
+++ apps/client/src/app/map/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import KakaoMap from "@/components/KakaoMap";
-import { Button } from "@place-note/ui";
 
 export default function Map() {
   return (
-    <div>
+    <>
+      <div className="flex flex-col px-12">
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요."
+          className="p-4 border rounded-lg border-gray-300 w-xl"
+        />
+      </div>
       <KakaoMap />
-      <Button />
-    </div>
+    </>
   );
 }
--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,25 +1,58 @@
 "use client";
 
-import Link from "next/link";
-import { Button } from "@place-note/ui";
-import { useGeoLocation } from "@place-note/api";
+import { useRouter } from "next/navigation";
+import Button from "@/components/Button";
+import Modal from "@/components/Modal";
+import { useModal } from "@/hooks/useModal";
 
 export default function Home() {
-  const { location, errorMessage } = useGeoLocation();
+  const { isOpen, openModal, closeModal } = useModal();
+  const clickButton = () => {
+    openModal();
+  };
+
+  const router = useRouter();
+  const clickRouter = () => {
+    router.push("/map");
+  };
 
   return (
-    <main className="flex justify-center items-center h-screen ">
-      <div className="flex justify-center items-center">
-        <Link href={"/map"}>지도</Link>
-        <Button />
-        {location ? (
-          <p>{/* 현재 위치: {location.latitude}, {location.longitude} */}</p>
-        ) : errorMessage ? (
-          <p className="text-red-500">위치 정보 에러: {errorMessage}</p>
-        ) : (
-          <p>위치 정보를 가져오는 중...</p>
-        )}
-      </div>
+    <main className="flex flex-col items-center justify-center h-screen">
+      <section className="flex flex-col justify-center gap-8">
+        <div className="flex flex-col gap-4 justify-center px-3 text-center">
+          <span className="font-bold text-4xl text-amber-500">
+            📝 Place Note
+          </span>
+          <p className="text-lg text-gray-600">
+            장소를 기록하고 쌓아가는 나만의 장소 노트
+          </p>
+        </div>
+        <Button onClick={clickButton} size="lg">
+          시작하기
+        </Button>
+      </section>
+
+      <Modal
+        isOpen={isOpen}
+        title="권한 요청"
+        closeModal={closeModal}
+        size="sm"
+      >
+        <div className="flex flex-col">
+          <span className="text-center">
+            현재 위치 접근에 동의하시겠습니까?
+          </span>
+          <div className="flex flex-col gap-2 justify-between mt-8">
+            <Button onClick={clickRouter}>동의</Button>
+            <Button
+              onClick={() => alert("위치 접근을 거부하였습니다.")}
+              variant="outline"
+            >
+              동의하지 않음
+            </Button>
+          </div>
+        </div>
+      </Modal>
     </main>
   );
 }
